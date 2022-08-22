23f9532f77a1c3a3f0dcf36ef8fbb16ffb30063b
jeanPierre3 <jean_pierre3@outlook.com>
2022-08-21 21:14:05 -0300

    21 agosto

--- ProyectoIntegradorJP/src/app/app.module.ts
+++ ProyectoIntegradorJP/src/app/app.module.ts
@@ -1,31 +1,46 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HeaderComponent } from './components/header/header.component';
 import { LogoAPComponent } from './components/logo-ap/logo-ap.component';
-import { SocialesComponent } from './components/sociales/sociales.component';
+
 import { BanerComponent } from './components/baner/baner.component';
 import { AcercaDeComponent } from './components/acerca-de/acerca-de.component';
 import { ExperienciaComponent } from './components/experiencia/experiencia.component';
 import { EducacionComponent } from './components/educacion/educacion.component';
+import { NgCircleProgressModule } from 'ng-circle-progress';
+import { HysComponent } from './components/hys/hys.component';
+import { ProyectoComponent } from './components/proyecto/proyecto.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     LogoAPComponent,
-    SocialesComponent,
+
     BanerComponent,
     AcercaDeComponent,
     ExperienciaComponent,
-    EducacionComponent
+    EducacionComponent,
+    HysComponent,
+    ProyectoComponent,
+    FooterComponent,
+
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    NgCircleProgressModule.forRoot({
+      })
   ],
+   
+
   providers: [],
   bootstrap: [AppComponent]
 })
--- ProyectoIntegradorJP/src/app/components/hys/hys.component.spec.ts
+++ ProyectoIntegradorJP/src/app/components/hys/hys.component.spec.ts
@@ -1,18 +1,18 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { SocialesComponent } from './sociales.component';
+import { HysComponent } from './hys.component';
 
-describe('SocialesComponent', () => {
-  let component: SocialesComponent;
-  let fixture: ComponentFixture<SocialesComponent>;
+describe('HysComponent', () => {
+  let component: HysComponent;
+  let fixture: ComponentFixture<HysComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ SocialesComponent ]
+      declarations: [ HysComponent ]
     })
     .compileComponents();
 
-    fixture = TestBed.createComponent(SocialesComponent);
+    fixture = TestBed.createComponent(HysComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
